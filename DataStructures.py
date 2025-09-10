83e82aac96809371bfd67b050d31f4cc68279381
jkurtis640-droid <jkurtis640@phm.k12.in.us>
2025-09-10 08:47:31 -0400

    Update DataStructures.py

--- DataStructures.py
+++ DataStructures.py
@@ -7,4 +7,8 @@ for i in range(2):
 color = ("purple", "orange", "yellow", "green", "blue")
 print(color)
 ## Does not run
-
+## Lists
+number = ["1","2","3","4","5","6","7","8","9","10"]
+print(number[0])
+print(number[4])
+print(number[9])
